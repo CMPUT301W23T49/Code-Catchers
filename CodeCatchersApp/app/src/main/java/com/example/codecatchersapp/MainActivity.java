94b237e8305adcc25cc0a6d3da7d3a97ce093cdb
“Noah <noah.mk.silva@gmail.com>
2023-02-25 02:13:28 -0700

    Noah 2 Social Menu and Browse Users XML Working on querying firestore db to return usernames
    incomplete

--- CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/MainActivity.java
+++ CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
