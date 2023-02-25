c8b9389638e1ee33db2a73f63cf6c8fed0f535a4
kyle <kkarpysh@ualberta.ca>
2023-02-24 19:25:11 -0700

    test

--- CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/MainActivity.java
+++ CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.codecatchersapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.Manifest;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private Button mScanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScanButton = findViewById(R.id.scan_button);
+        mScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                } else {
+                    startScannerActivity();
+                }
+            }
+        });
+    }
+
+    private void startScannerActivity() {
+        Intent intent = new Intent(MainActivity.this, ScannerActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startScannerActivity();
+            } else {
+                Toast.makeText(this, "Camera permission is required to scan QR codes", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
