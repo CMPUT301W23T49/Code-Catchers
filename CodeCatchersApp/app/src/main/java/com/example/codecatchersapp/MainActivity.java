a41b6ed0ab53698519d0004006bd4ba18e2aaa78
nteglauer <nteglauer@gmail.com>
2023-02-26 16:03:20 -0700

    update user account

---
+++ CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.codecatchersapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+    }
+}
