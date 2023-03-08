7be7a9da559b5e8bd43c41662d3cb640db021c56
kyle <kkarpysh@ualberta.ca>
2023-03-08 16:20:21 -0700

    test

--- CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/ScannerActivity.java
+++ CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/ScannerActivity.java
@@ -1,56 +1,143 @@
 package com.example.codecatchersapp;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.zxing.Result;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class ScannerActivity extends AppCompatActivity {
-    private CodeScanner mCodeScanner;
+    private static final String TAG = "ScannerActivity";
+    private static final int CAMERA_REQUEST_CODE = 100; // https://stackoverflow.com/questions/38507965/what-does-camera-request-code-mean-in-android
+    private static final int REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 101;
+    private CodeScanner codeScanner;
+    private CodeScannerView scannerView;
+    private ImageView screenshotView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scanner);
-        CodeScannerView scannerView = findViewById(R.id.scanner_view);
-        mCodeScanner = new CodeScanner(this, scannerView);
-        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+        scannerView = findViewById(R.id.scanner_view);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        } else {
+            startScanner();
+        }
+    }
+
+    private void startScanner() {
+        codeScanner = new CodeScanner(this, scannerView);
+        screenshotView = findViewById(R.id.screenshot_view);
+        codeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
             public void onDecoded(@NonNull Result result) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        String qrCodeText = result.getText();
-                        Toast.makeText(ScannerActivity.this, qrCodeText, Toast.LENGTH_LONG).show();
+                        takeScreenshot();
+                        Snackbar.make(scannerView, result.getText(), Snackbar.LENGTH_LONG).show();
                     }
                 });
             }
         });
+
+        /*
+        * TEMP TEST CODE
+        * REPLACE WITH -> ON ERROR DISPLAY SCAN ERROR VIEW -> WAIT FOR USER TO TAP -> GO BACK TO START SCANNER
+        */
+        codeScanner.setErrorCallback(error -> {
+            Log.e(TAG, "Scan error", error);
+            Toast.makeText(ScannerActivity.this, "Scan error: " + error.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        });
+
         scannerView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 mCodeScanner.startPreview();
+                codeScanner.startPreview();
             }
         });
     }
 
+    private void takeScreenshot() {
+        // CHECK TO WRITE TO STORAGE PERMS
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_WRITE_EXTERNAL_STORAGE);
+            return;
+        }
+
+        View rootView = getWindow().getDecorView().findViewById(android.R.id.content);
+        rootView.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(rootView.getDrawingCache());
+        rootView.setDrawingCacheEnabled(false);
+
+        // SAVE BITMAP TO FILE
+        try {
+            File directory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "tmp_qrcodes");
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss", Locale.US);  // might have to get locale from device, not sure
+            String fileName = "QR_" + dateFormat.format(new Date()) + ".png";
+            File file = new File(directory, fileName);
+            FileOutputStream fos = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+
+            Toast.makeText(this, "Screenshot saved to " + file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     protected void onResume() {
         super.onResume();
-        mCodeScanner.startPreview();
+        if (codeScanner != null) {
+            codeScanner.startPreview();
+        }
     }
 
     @Override
     protected void onPause() {
-        mCodeScanner.releaseResources();
+        if (codeScanner != null) {
+            codeScanner.releaseResources();
+        }
         super.onPause();
+    }
+
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startScanner();
+            } else {
+                Toast.makeText(this, "Camera permission required to scan QR codes", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
 }
