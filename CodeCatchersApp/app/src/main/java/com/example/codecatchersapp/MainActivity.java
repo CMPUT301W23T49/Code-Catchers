9803ccde572313dde424a494532af2d927148852
venetiaz <zhashekendeguia@gmail.com>
2023-03-11 21:58:30 -0700

    Fixed database paths

--- CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/MainActivity.java
+++ CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(this, MonInfoActivity.class);
         startActivity(intent);
