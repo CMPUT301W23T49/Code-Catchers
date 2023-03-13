d1c707396dd74fd855fc9d5ffd3a33523bebb50d
noahmsilva <73564799+noahmsilva@users.noreply.github.com>
2023-03-13 13:58:11 -0600

    Leaderboards.java Javadocs

--- CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/Leaderboards.java
+++ CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/Leaderboards.java
@@ -1,6 +1,17 @@
+/**
+ * The Leaderboards class contains methods for
+ * a Leaderboard object so it can be stored in an Arraylist<Leaderboards>
+ * @author [Noah Silva]
+ * @version 1.0
+ * @since [Monday March 13 2023]
+ */
 package com.example.codecatchersapp;
 
-
+/**
+ * Leaderboards implements ArrayAdapter<Leaderboards>.
+ * It is responsible for being a Leaderboard object, returning
+ *  and comparing values when needed
+ */
 public class Leaderboards implements Comparable<Leaderboards> {
 
     private String username;
@@ -11,7 +22,11 @@ public class Leaderboards implements Comparable<Leaderboards> {
         this.username = username;
         this.score = score;
     }
-
+    
+    /**
+     * Gets username
+     * @return username of this object
+     */
     public String getUsername(){
         return username;
     }
@@ -22,11 +37,19 @@ public class Leaderboards implements Comparable<Leaderboards> {
     }
      */
 
+    /**
+     * Gets score value as a string
+     * @return score of this object
+     */
     public String getScoreStringRepresentation(){
         String scoreString = score.toString();
         return scoreString;
     }
 
+    /**
+     * Compares Leaderboards objects based upon their score value
+     * @return -1 if this Leaderboard is larger than one it is being compared to, 0 if even, 1 otherwise
+     */
     @Override
     public int compareTo(Leaderboards otherScore) {
         if (Integer.parseInt(this.score) > Integer.parseInt(otherScore.score)){
