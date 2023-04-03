28c409a1ccc3bb9623350a4b7d1a1f3910c87dad
kyle <kkarpysh@ualberta.ca>
2023-04-02 18:54:45 -0600

    :)

--- CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/QROptionsActivity.java
+++ CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/QROptionsActivity.java
@@ -26,6 +26,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
 
@@ -74,7 +75,7 @@ public class QROptionsActivity extends AppCompatActivity {
 
         // Get score of scanned monster
         try {
-            Score score = new Score(monsterHash);
+            Score score = new Score(binaryHash);
             displayScore = score.getScore();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
