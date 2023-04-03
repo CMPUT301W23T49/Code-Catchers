78afa111a42414a32dfe8c92c991b928d88f505c
Jcatz13 <58762789+JcatZ13@users.noreply.github.com>
2023-04-03 16:24:16 -0600

    small househeeping

--- CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/UserSettingsActivity.java
+++ CodeCatchersApp/app/src/main/java/com/example/codecatchersapp/UserSettingsActivity.java
@@ -1,9 +1,3 @@
-/**
- * a class
- * @author CMPUT301W23T49
- * @version 1.0
- * @since [Monday April 3]
- */
 package com.example.codecatchersapp;
 
 import android.content.Intent;
@@ -11,15 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 /**
- The UserSettingsActivity class is responsible for displaying and allowing the user to edit their settings.
- It extends the AppCompatActivity class and overrides the onCreate() method.
+ UserSettingsActivity allows the user to change their contact info.
  */
 public class UserSettingsActivity extends AppCompatActivity {
     private FloatingActionButton backButton;
@@ -30,11 +29,10 @@ public class UserSettingsActivity extends AppCompatActivity {
     private String deviceID;
     private String userName;
     private String contactInfo;
-
     /**
-     This method is called when the activity is created.
-     It initializes the views and sets the corresponding listeners.
-     @param savedInstanceState A Bundle object containing the activity's previously saved state.
+     * onCreate sets the content view, gets the intent extras, and sets the text views.
+     * It also sets the on click listeners for the back button, cancel button, and save button.
+     * @param savedInstanceState
      */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,11 +40,16 @@ public class UserSettingsActivity extends AppCompatActivity {
         // Set content view
         setContentView(R.layout.user_settings);
 
+
+
         // Get intent extras
         Intent intent = getIntent();
         deviceID = intent.getStringExtra("deviceID");
         userName = intent.getStringExtra("userName");
         contactInfo = intent.getStringExtra("contactInfo");
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = db.collection("PlayerDB");
 
         // Get the views for UserSettingsActivity
         backButton = findViewById(R.id.back_button);
@@ -62,13 +65,49 @@ public class UserSettingsActivity extends AppCompatActivity {
         // Set on click listeners
         backButton.setOnClickListener(new View.OnClickListener() {
             /**
-             * This method is called when the back button is clicked. It navigates back to the
-             * previous activity.
-             * @param view The clicked view.
+             * When the back button is clicked, the user is taken back to the MyProfileActivity.
+             * @param view
              */
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * When the cancel button is clicked, the user is taken back to the MyProfileActivity.
+             * @param view
+             */
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * When the save button is clicked, the user's contact info is updated in the database.
+             * @param view
+             */
+            @Override
+            public void onClick(View view) {
+                collectionReference.document(deviceID).update("contactInfo", editContactInfo.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            /**
+                             * When the update is complete, the user is taken back to the MyProfileActivity.
+                             * @param task
+                             */
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(UserSettingsActivity.this, MyProfileActivity.class);
+                                    startActivity(intent);
+                                    Toast.makeText(UserSettingsActivity.this, "Contact info updated successfully", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(UserSettingsActivity.this, "Failed to update contact info", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
